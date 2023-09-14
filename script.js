0fd57e0c9af9f9ab6d44f02980a584f063044ea0
dave8git <dave8git@gmail.com>
2023-09-14 02:18:29 +0200

    Add gallery

--- script.js
+++ script.js
@@ -22,3 +22,56 @@ function onClick(element) {
   function w3_close() {
       mySidebar.style.display = "none";
   }
+
+  const squares = document.querySelectorAll('.square');
+
+squares.forEach((square, index) => {
+    setTimeout(() => {
+        square.style.transform = 'translateX(0)';
+        square.style.transform = 'translate(-50%, -50%)';
+    }, 1000 * index);
+});
+
+// JavaScript for the image gallery with lightbox
+
+const gallery = document.querySelector('.photo-gallery');
+const lightbox = document.querySelector('.lightbox');
+const lightboxContent = document.querySelector('.lightbox-content');
+const closeBtn = document.querySelector('.close');
+
+// Function to open the lightbox with a specific image
+function openLightbox(imageSrc) {
+    lightboxContent.innerHTML = `<img src="${imageSrc}" alt="">`;
+    lightbox.style.display = 'block';
+}
+
+// Function to close the lightbox
+function closeLightbox() {
+    lightbox.style.display = 'none';
+}
+
+// Attach click event listeners to each thumbnail
+gallery.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        const imageSrc = e.target.src;
+        openLightbox(imageSrc);
+    }
+});
+
+// Close the lightbox when the close button is clicked
+closeBtn.addEventListener('click', closeLightbox);
+
+// Close the lightbox when clicking outside the image
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        closeLightbox();
+    }
+});
+
+// Close the lightbox with the Escape key
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        closeLightbox();
+    }
+});
+
